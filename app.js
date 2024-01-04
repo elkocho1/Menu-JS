35732352c34d686b92cfa078ff28d478cb36fe4c
Michael <michaell.hirt@gmail.com>
2024-01-04 11:59:19 -0500

    add filter item fuction

--- app.js
+++ app.js
@@ -74,9 +74,21 @@ const menu = [{
 
 const sectionCenter = document.querySelector(".section-center");
 
+const filterBtns = document.querySelectorAll(".filter-btn");
+
 window.addEventListener("DOMContentLoaded", function () {
   displayMenuItems(menu)
 });
+
+//filter items
+filterBtns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const category = e.currentTarget.dataset.id;
+  });
+});
+
+
+
 
 function displayMenuItems(menuItems) {
 
