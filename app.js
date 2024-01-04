e53a3cf84887aec0592ff9a24091f2af1c017005
Michael <michaell.hirt@gmail.com>
2024-01-04 12:14:37 -0500

    add the menuItem function

--- app.js
+++ app.js
@@ -84,9 +84,18 @@ window.addEventListener("DOMContentLoaded", function () {
 filterBtns.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
     const category = e.currentTarget.dataset.id;
-    const menuCategory = menu.filter(function (menuItems) {
-      return menuItems;
+    const menuCategory = menu.filter(function (menuItem) {
+      //  console.log(menuItem.category);
+      if (menuItem.category === category) {
+        return menuItem;
+      }
     });
+    // console.log(menuCategory);
+    if (category === "all") {
+      displayMenuItems(menu);
+    } else {
+      displayMenuItems(menuCategory);
+    }
   });
 });
 
